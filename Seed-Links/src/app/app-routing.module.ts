0f6b8ef2f4cd2140dd2e0dde6fa146df8f5345d5
Dwan13 <dwanveloza@gmail.com>
2021-07-28 15:25:29 -0500

    modularizando la Landing Page

--- Seed-Links/src/app/app-routing.module.ts
+++ Seed-Links/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { PageNotFountComponent } from './page-not-fount/page-not-fount.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,    
+    children:[
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch:'full'
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+        //component:HomeComponent
+      }
+    ]
+  },   
+  {
+    path: '**',
+    component: PageNotFountComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Seed-Links/src/app/app.module.ts
+++ Seed-Links/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { PageNotFountComponent } from './page-not-fount/page-not-fount.component';
+import { SharedModule } from './shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    PageNotFountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    FormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
